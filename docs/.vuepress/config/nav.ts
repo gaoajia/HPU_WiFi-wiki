eb21d6db7deb70f7f882bb1f7ae31da3d26db1eb
gaoajia <gaoajia@qq.com>
2022-09-06 23:31:49 +0800

    update

--- docs/.vuepress/config/nav.ts
+++ docs/.vuepress/config/nav.ts
@@ -4,40 +4,19 @@ import { NavItem } from "vuepress/config";
 export default <Array<NavItem>>[
   { text: "Home", link: "/" },
   {
-    text: "配置",
+    text: "目录",
     items: [
       {
-        text: "项目配置",
+        text: "开始",
         link: "/start/",
       },
       {
-        text: "部署",
+        text: "故障a",
         link: "/deploy/",
       },
       {
         text: "更多参考",
         link: "/more/",
-      },
-    ],
-  },
-  {
-    text: "友情链接",
-    items: [
-      {
-        text: "vuepress",
-        link: "https://vuepress.vuejs.org/",
-      },
-      {
-        text: "vuepress-theme-vdoing",
-        link: "https://doc.xugaoyi.com/",
-      },
-      {
-        text: "U2SB",
-        link: "https://www.u2sb.com/",
-      },
-      {
-        text: "MonoLogueChi",
-        link: "https://blog.xxwhite.com/",
       },
     ],
   },
